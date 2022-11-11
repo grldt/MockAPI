f9f83a10f3afbc24cc2af65dfe7a68e9c897d0a2
grldt <geraldstephanus@gmail.com>
2022-11-11 16:03:19 +0700

    Update MockAPIApp.swift

--- MockAPI/MockAPIApp.swift
+++ MockAPI/MockAPIApp.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 @main
 struct MockAPIApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ProductListView()
