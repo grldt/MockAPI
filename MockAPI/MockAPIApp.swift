a123df935f78c69066a200bf41526ed1aae03119
grldt <geraldstephanus@gmail.com>
2022-11-13 20:14:58 +0700

    Lower minimum version to 15.0

--- MockAPI/MockAPIApp.swift
+++ MockAPI/MockAPIApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct MockAPIApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ProductListView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
